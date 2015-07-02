79014690fe15c4e7ec9e6719a7a94cfffb28f0f2
idlesong <idlesong@gmail.com>
2015-07-02 17:51:26 +0800

    add root router

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
+  root :to => 'nodes#index', :as => 'nodes'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
